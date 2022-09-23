a9fd4701b0a347babd13aee90bfaae02634a90fe
Stöffler Mario <52000392@edu.campus02.at>
2022-09-23 17:10:15 +0200

    My First Class created; args as input parameter written in console

---
+++ MyFirstClass.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class MyFirstClass {
+// wir haben unsere erste Klasse erzeugt
+
+    public static void main(String[] args) {
+        // erste Ausgabe
+        System.out.println("Ich werde auf der Konsole angezeigt");
+
+        //args
+        System.out.println(Arrays.toString(args));
+    }
+
+
+}
