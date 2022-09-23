7562db4176683d5bf72cdf6a3d20391a773e1579
Stöffler Mario <52000392@edu.campus02.at>
2022-09-23 18:28:32 +0200

    Class Calculator created; declaration of simple variables + print out simple addition;

---
+++ Calculator.java
@@ -0,0 +1,13 @@
+public class Calculator {
+
+    public static void main(String[] args) {
+        int a = 349;
+        int b = 523;
+        int c = a + b;
+        String nameDesRechners = "Mario";
+
+        System.out.println("Mein "+nameDesRechners + " ist "  + c);
+
+
+    }
+}
