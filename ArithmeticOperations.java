8e496a304ebfc43f585646edd1e0734206ce53a3
Stöffler Mario <52000392@edu.campus02.at>
2022-09-24 11:53:25 +0200

    Class ArithmeticOperations created; and simple creations of all operating calculations;

---
+++ ArithmeticOperations.java
@@ -0,0 +1,15 @@
+public class ArithmeticOperations {
+
+    public static void main(String[] args) {
+        int a = 23;
+        int b = 768;
+
+        System.out.println("Ergebnis der Addition = " + (a + b));
+        System.out.println("Ergebnis der Subtraktion = " + (a - b));
+        System.out.println("Ergebnis der Multiplikation = " + (a * b));
+        System.out.println("Ergebnis der Division = " + (a / b));
+
+        System.out.println("Ergebnis der Modulo Rechnung = " + (a % b));
+
+    }
+}
